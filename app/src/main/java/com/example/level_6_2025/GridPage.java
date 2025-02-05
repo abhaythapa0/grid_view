330ecdb2b936c39a7ee01991c616dec42abb9e44
abhaythapa0 <134987530+abhaythapa0@users.noreply.github.com>
2025-02-05 07:21:39 +0545

    first commit

---
+++ app/src/main/java/com/example/level_6_2025/GridPage.java
@@ -0,0 +1,21 @@
+package com.example.level_6_2025;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GridPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_grid_page);
+
+
+    }
+}
